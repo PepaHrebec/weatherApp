c4c3f27a229cc500cb125817f4fa3b065afdb702
Josef Hrebec <josefhrebec02@gmail.com>
2022-10-20 14:01:32 +0200

    Add changing background

--- src/postWeather.js
+++ src/postWeather.js
@@ -12,32 +12,51 @@ function postWeather(location, temp, tempMin, describe) {
 }
 
 function weatherDescribe(weather) {
+  const backgrnd = document.querySelector("body");
   switch (weather) {
     case "Thunderstorm":
+      backgrnd.style.backgroundImage = "url('./../imgs/storm.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "There's a thunderstorm outside";
       break;
     case "Drizzle":
+      backgrnd.style.backgroundImage = "url('./../imgs/rain.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "There's a slight drizzle outside";
       break;
     case "Rain":
+      backgrnd.style.backgroundImage = "url('./../imgs/rain.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "It's raining outside";
       break;
     case "Snow":
+      backgrnd.style.backgroundImage = "url('./../imgs/snow.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "It's snowing outside";
       break;
     case "Clear":
+      backgrnd.style.backgroundImage = "url('./../imgs/clear.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "It's all clear outside";
       break;
     case "Clouds":
+      backgrnd.style.backgroundImage = "url('./../imgs/cloudy.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "It's a bit cloudy outside";
       break;
     case "Sand":
+      backgrnd.style.backgroundImage = "url('./../imgs/sand.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "There's sand outside";
       break;
     case "Err":
+      backgrnd.style.backgroundImage = "url('./../imgs/mars.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "There's a huge storm coming, better hide";
       break;
     default: //generic mist
+      backgrnd.style.backgroundImage = "url('./../imgs/mist.jpg')";
+      backgrnd.style.backgroundSize = "cover";
       return "It's a bit misty outside";
       break;
   }
